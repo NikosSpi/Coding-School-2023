0a918e9945043a0e505b7b173a84827a6c80af5a
nikos spiliopoulos <wpbyspi@gmail.com>
2023-01-14 18:49:05 +0200

    ex2 done

--- Session-05/Session-05/Choose.cs
+++ Session-05/Session-05/Choose.cs
@@ -7,20 +7,41 @@ namespace Session5
     {
 
 
-        public string GetB()
+        public int GetB()
         {
-            string name = "kostas";
-            string namereversed = "";
-            for (int i = name.Length - 1; i >= 0; i--)
+            int sum = 0;
+            int product = 1;
+            int integer, choose;
+
+            Console.WriteLine("Please give an ineteger: ");
+            integer = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please type 1 if you need to compute the sum or 2 if you need to compute the product: ");
+            choose = Convert.ToInt32(Console.ReadLine());
+            if (choose == 1)
             {
-                namereversed = namereversed + name[i];
+                for (int i = 0; i <= integer; i++)
+                {
+                    sum = sum + i;
+                }
+                return sum;
 
             }
+            if (choose == 2)
+            {
+                for (int j = 1; j <= integer +1; j++)
+                {
+                    product = product * j;
+                }
+                return product;
+            }
+            return integer;
+            
 
 
-            return namereversed;
+
 
         }
-
     }
 }
+ 
