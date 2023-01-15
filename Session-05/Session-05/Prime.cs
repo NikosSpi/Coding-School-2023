d0a4063acf8ba1270a354b01318ef11f5fdf70f3
nikos spiliopoulos <wpbyspi@gmail.com>
2023-01-15 03:16:08 +0200

    ex3 done

---
+++ Session-05/Session-05/Prime.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Session5
+{
+
+    public class Prime
+    {
+
+
+        public string GetC()
+        {
+            int divisors = 0;
+            int integer;
+
+            Console.WriteLine("Please give an integer to find the prime numbers: ");
+            integer = Convert.ToInt32(Console.ReadLine());
+
+            List<int> numbers = new List<int>();
+
+            for (int j = 1; j <= integer; j++)
+            {
+                divisors = 0;
+                for (int i = 1; i <= j; i++)
+                {
+                    if (j % i == 0)
+                    {
+                        divisors++;
+                    }
+
+                }
+                if (divisors == 2)
+                {
+                    numbers.Add(j);
+                }
+
+
+            }
+
+            foreach (int item in numbers)
+            {
+                Console.WriteLine("prime number: " + item);
+            }
+
+            return "these are all the prime numbers";
+
+        }
+    }
+}
