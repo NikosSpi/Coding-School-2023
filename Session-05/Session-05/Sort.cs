ef35f6b148472efa1eea75b50061a91a35bd9ff1
nikos spiliopoulos <wpbyspi@gmail.com>
2023-01-15 04:57:53 +0200

    sort change

--- Session-05/Session-05/Sort.cs
+++ Session-05/Session-05/Sort.cs
@@ -15,9 +15,9 @@ namespace Session5
 
             for (int i = 0; i < 10; i++)
             {
-                for (int j = 0; j < 10; j++)
+                for (int j = i+1; j < 10; j++)
                 {
-                    if (array[i] < array[j])
+                    if (array[i] > array[j])
                     {
                         reverse = array[i];
                         array[i] = array[j];
