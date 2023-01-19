40aa57046a4637de624acab6fb4452b0998cde70
nikos spiliopoulos <wpbyspi@gmail.com>
2023-01-19 13:46:31 +0200

    stage 1

---
+++ Session-09/Session-09/Form1.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace Session_09
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ctrlDisplay = new System.Windows.Forms.TextBox();
+            this.ctrlOne = new System.Windows.Forms.Button();
+            this.ctrlTwo = new System.Windows.Forms.Button();
+            this.ctrlThree = new System.Windows.Forms.Button();
+            this.ctrlFour = new System.Windows.Forms.Button();
+            this.ctrlFive = new System.Windows.Forms.Button();
+            this.ctrlSix = new System.Windows.Forms.Button();
+            this.ctrlSeven = new System.Windows.Forms.Button();
+            this.ctrlEight = new System.Windows.Forms.Button();
+            this.ctrlNine = new System.Windows.Forms.Button();
+            this.ctrlZero = new System.Windows.Forms.Button();
+            this.ctrlDecimalPoint = new System.Windows.Forms.Button();
+            this.ctrlEquals = new System.Windows.Forms.Button();
+            this.ctrlAddition = new System.Windows.Forms.Button();
+            this.ctrlSubtraction = new System.Windows.Forms.Button();
+            this.ctrlMultiplication = new System.Windows.Forms.Button();
+            this.ctrlDevision = new System.Windows.Forms.Button();
+            this.ctrlSquareRoot = new System.Windows.Forms.Button();
+            this.ctrlPower = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ctrlDisplay
+            // 
+            this.ctrlDisplay.AccessibleDescription = "";
+            this.ctrlDisplay.AccessibleName = "";
+            this.ctrlDisplay.Font = new System.Drawing.Font("Arial Narrow", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.ctrlDisplay.Location = new System.Drawing.Point(549, 67);
+            this.ctrlDisplay.Name = "ctrlDisplay";
+            this.ctrlDisplay.Size = new System.Drawing.Size(378, 76);
+            this.ctrlDisplay.TabIndex = 0;
+            this.ctrlDisplay.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // ctrlOne
+            // 
+            this.ctrlOne.Location = new System.Drawing.Point(549, 212);
+            this.ctrlOne.Name = "ctrlOne";
+            this.ctrlOne.Size = new System.Drawing.Size(97, 66);
+            this.ctrlOne.TabIndex = 1;
+            this.ctrlOne.Text = "1";
+            this.ctrlOne.UseVisualStyleBackColor = true;
+            // 
+            // ctrlTwo
+            // 
+            this.ctrlTwo.Location = new System.Drawing.Point(652, 212);
+            this.ctrlTwo.Name = "ctrlTwo";
+            this.ctrlTwo.Size = new System.Drawing.Size(102, 66);
+            this.ctrlTwo.TabIndex = 2;
+            this.ctrlTwo.Text = "2";
+            this.ctrlTwo.UseVisualStyleBackColor = true;
+            // 
+            // ctrlThree
+            // 
+            this.ctrlThree.Location = new System.Drawing.Point(760, 212);
+            this.ctrlThree.Name = "ctrlThree";
+            this.ctrlThree.Size = new System.Drawing.Size(94, 66);
+            this.ctrlThree.TabIndex = 3;
+            this.ctrlThree.Text = "3";
+            this.ctrlThree.UseVisualStyleBackColor = true;
+            // 
+            // ctrlFour
+            // 
+            this.ctrlFour.Location = new System.Drawing.Point(549, 301);
+            this.ctrlFour.Name = "ctrlFour";
+            this.ctrlFour.Size = new System.Drawing.Size(97, 62);
+            this.ctrlFour.TabIndex = 4;
+            this.ctrlFour.Text = "4";
+            this.ctrlFour.UseVisualStyleBackColor = true;
+            // 
+            // ctrlFive
+            // 
+            this.ctrlFive.Location = new System.Drawing.Point(652, 302);
+            this.ctrlFive.Name = "ctrlFive";
+            this.ctrlFive.Size = new System.Drawing.Size(102, 61);
+            this.ctrlFive.TabIndex = 5;
+            this.ctrlFive.Text = "5";
+            this.ctrlFive.UseVisualStyleBackColor = true;
+            // 
+            // ctrlSix
+            // 
+            this.ctrlSix.Location = new System.Drawing.Point(760, 301);
+            this.ctrlSix.Name = "ctrlSix";
+            this.ctrlSix.Size = new System.Drawing.Size(94, 62);
+            this.ctrlSix.TabIndex = 6;
+            this.ctrlSix.Text = "6";
+            this.ctrlSix.UseVisualStyleBackColor = true;
+            // 
+            // ctrlSeven
+            // 
+            this.ctrlSeven.Location = new System.Drawing.Point(549, 383);
+            this.ctrlSeven.Name = "ctrlSeven";
+            this.ctrlSeven.Size = new System.Drawing.Size(97, 63);
+            this.ctrlSeven.TabIndex = 7;
+            this.ctrlSeven.Text = "7";
+            this.ctrlSeven.UseVisualStyleBackColor = true;
+            // 
+            // ctrlEight
+            // 
+            this.ctrlEight.Location = new System.Drawing.Point(652, 383);
+            this.ctrlEight.Name = "ctrlEight";
+            this.ctrlEight.Size = new System.Drawing.Size(102, 63);
+            this.ctrlEight.TabIndex = 8;
+            this.ctrlEight.Text = "8";
+            this.ctrlEight.UseVisualStyleBackColor = true;
+            // 
+            // ctrlNine
+            // 
+            this.ctrlNine.Location = new System.Drawing.Point(760, 383);
+            this.ctrlNine.Name = "ctrlNine";
+            this.ctrlNine.Size = new System.Drawing.Size(94, 63);
+            this.ctrlNine.TabIndex = 9;
+            this.ctrlNine.Text = "9";
+            this.ctrlNine.UseVisualStyleBackColor = true;
+            // 
+            // ctrlZero
+            // 
+            this.ctrlZero.Location = new System.Drawing.Point(652, 465);
+            this.ctrlZero.Name = "ctrlZero";
+            this.ctrlZero.Size = new System.Drawing.Size(102, 55);
+            this.ctrlZero.TabIndex = 10;
+            this.ctrlZero.Text = "0";
+            this.ctrlZero.UseVisualStyleBackColor = true;
+            // 
+            // ctrlDecimalPoint
+            // 
+            this.ctrlDecimalPoint.Location = new System.Drawing.Point(760, 465);
+            this.ctrlDecimalPoint.Name = "ctrlDecimalPoint";
+            this.ctrlDecimalPoint.Size = new System.Drawing.Size(94, 55);
+            this.ctrlDecimalPoint.TabIndex = 11;
+            this.ctrlDecimalPoint.Text = ",";
+            this.ctrlDecimalPoint.UseVisualStyleBackColor = true;
+            // 
+            // ctrlEquals
+            // 
+            this.ctrlEquals.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ctrlEquals.Location = new System.Drawing.Point(860, 465);
+            this.ctrlEquals.Name = "ctrlEquals";
+            this.ctrlEquals.Size = new System.Drawing.Size(67, 55);
+            this.ctrlEquals.TabIndex = 12;
+            this.ctrlEquals.Text = "=";
+            this.ctrlEquals.UseVisualStyleBackColor = false;
+            // 
+            // ctrlAddition
+            // 
+            this.ctrlAddition.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ctrlAddition.Location = new System.Drawing.Point(860, 383);
+            this.ctrlAddition.Name = "ctrlAddition";
+            this.ctrlAddition.Size = new System.Drawing.Size(67, 63);
+            this.ctrlAddition.TabIndex = 13;
+            this.ctrlAddition.Text = "+";
+            this.ctrlAddition.UseVisualStyleBackColor = false;
+            // 
+            // ctrlSubtraction
+            // 
+            this.ctrlSubtraction.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ctrlSubtraction.Location = new System.Drawing.Point(860, 302);
+            this.ctrlSubtraction.Name = "ctrlSubtraction";
+            this.ctrlSubtraction.Size = new System.Drawing.Size(67, 61);
+            this.ctrlSubtraction.TabIndex = 14;
+            this.ctrlSubtraction.Text = "-";
+            this.ctrlSubtraction.UseVisualStyleBackColor = false;
+            // 
+            // ctrlMultiplication
+            // 
+            this.ctrlMultiplication.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ctrlMultiplication.Location = new System.Drawing.Point(860, 212);
+            this.ctrlMultiplication.Name = "ctrlMultiplication";
+            this.ctrlMultiplication.Size = new System.Drawing.Size(67, 66);
+            this.ctrlMultiplication.TabIndex = 15;
+            this.ctrlMultiplication.Text = "✕";
+            this.ctrlMultiplication.UseVisualStyleBackColor = false;
+            // 
+            // ctrlDevision
+            // 
+            this.ctrlDevision.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ctrlDevision.Location = new System.Drawing.Point(860, 144);
+            this.ctrlDevision.Name = "ctrlDevision";
+            this.ctrlDevision.Size = new System.Drawing.Size(67, 62);
+            this.ctrlDevision.TabIndex = 16;
+            this.ctrlDevision.Text = "÷";
+            this.ctrlDevision.UseVisualStyleBackColor = false;
+            // 
+            // ctrlSquareRoot
+            // 
+            this.ctrlSquareRoot.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ctrlSquareRoot.Location = new System.Drawing.Point(760, 144);
+            this.ctrlSquareRoot.Name = "ctrlSquareRoot";
+            this.ctrlSquareRoot.Size = new System.Drawing.Size(94, 62);
+            this.ctrlSquareRoot.TabIndex = 17;
+            this.ctrlSquareRoot.Text = "√";
+            this.ctrlSquareRoot.UseVisualStyleBackColor = false;
+            // 
+            // ctrlPower
+            // 
+            this.ctrlPower.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ctrlPower.Location = new System.Drawing.Point(660, 144);
+            this.ctrlPower.Name = "ctrlPower";
+            this.ctrlPower.Size = new System.Drawing.Size(94, 62);
+            this.ctrlPower.TabIndex = 18;
+            this.ctrlPower.Text = "x^";
+            this.ctrlPower.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(928, 549);
+            this.Controls.Add(this.ctrlPower);
+            this.Controls.Add(this.ctrlSquareRoot);
+            this.Controls.Add(this.ctrlDevision);
+            this.Controls.Add(this.ctrlMultiplication);
+            this.Controls.Add(this.ctrlSubtraction);
+            this.Controls.Add(this.ctrlAddition);
+            this.Controls.Add(this.ctrlEquals);
+            this.Controls.Add(this.ctrlDecimalPoint);
+            this.Controls.Add(this.ctrlZero);
+            this.Controls.Add(this.ctrlNine);
+            this.Controls.Add(this.ctrlEight);
+            this.Controls.Add(this.ctrlSeven);
+            this.Controls.Add(this.ctrlSix);
+            this.Controls.Add(this.ctrlFive);
+            this.Controls.Add(this.ctrlFour);
+            this.Controls.Add(this.ctrlThree);
+            this.Controls.Add(this.ctrlTwo);
+            this.Controls.Add(this.ctrlOne);
+            this.Controls.Add(this.ctrlDisplay);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox ctrlDisplay;
+        private Button ctrlOne;
+        private Button ctrlTwo;
+        private Button ctrlThree;
+        private Button ctrlFour;
+        private Button ctrlFive;
+        private Button ctrlSix;
+        private Button ctrlSeven;
+        private Button ctrlEight;
+        private Button ctrlNine;
+        private Button ctrlZero;
+        private Button ctrlDecimalPoint;
+        private Button ctrlEquals;
+        private Button ctrlAddition;
+        private Button ctrlSubtraction;
+        private Button ctrlMultiplication;
+        private Button ctrlDevision;
+        private Button ctrlSquareRoot;
+        private Button ctrlPower;
+    }
+}
